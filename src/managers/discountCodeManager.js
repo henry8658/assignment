5047666e7849903d85f1d8e7a07e91dbe33fe6cc
Chuanyui Teh <evanyui@hotmail.com>
2020-05-02 17:57:29 -0700

    use idiomatic javascript

--- src/managers/discountCodeManager.js
+++ src/managers/discountCodeManager.js
@@ -3,13 +3,15 @@ export default class DiscountCodeManager
     constructor(){
         this.discountCodeDB = new Array();
     }
+    
     pushDiscount(discount)
     {
         this.discountCodeDB.push(discount);
     }
+    
     getDiscount()
     {
-        if (this.discountCodeDB.length == 0)
+        if (!this.discountCodeDB.length)
         {
             return false;
         } else
