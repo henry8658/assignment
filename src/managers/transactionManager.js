f6933ff6397c9d5c218f30955c2c8681b5bc5fee
Chuanyui Teh <evanyui@hotmail.com>
2020-05-02 17:58:30 -0700

    New lines

--- src/managers/transactionManager.js
+++ src/managers/transactionManager.js
@@ -6,26 +6,32 @@ export default class TransactionManager
         this.totalPurchaseCount = 0;
         this.totalDiscountPurchaseCount = 0;
     }
+    
     setN(nth)
     {
         this.nthTransaction = Number(nth);
     }
+    
     updatePurchaseCount()
     {
         this.totalPurchaseCount++;
     }
+    
     updateDiscountPurchaseCount()
     {
         this.totalDiscountPurchaseCount++;
     }
+    
     getN()
     {
         return this.nthTransaction;
     }
+    
     getTotalPurchaseCount()
     {
         return this.totalPurchaseCount;
     }
+    
     getDiscountPurchaseCount()
     {
         return this.totalDiscountPurchaseCount;
